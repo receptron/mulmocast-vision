e930cfa5298a67de7f0496268f79f67b99e0e8e2
isamu <isamu@to-kyo.to>
2025-08-21 11:02:40 +0900

    update

--- src/presentationHandlers/html.ts
+++ src/presentationHandlers/html.ts
@@ -11,48 +11,48 @@ const readFile = (filename: string) => {
 const filePath = (filename: string) => {
   return path.resolve(rootDir, "./assets/html/", `${filename}.html`);
 };
-
-const now = Date.now();
-
-const getOutfile = (index: number) => path.resolve(rootDir, "outdir", String(now), `${index}.png`);
-
 const mkdir = (dirPath: string) => {
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath, { recursive: true });
   }
 };
+
+const now = Date.now();
+export const baseDir = path.resolve(rootDir, "outdir", String(now));
+
+const getOutfile = (index: number) => path.resolve(baseDir, `${index}.png`);
+
 mkdir(path.resolve(rootDir, "outdir", String(now)));
 
-const renderPage = (body, args) => {
-  return nunjucks.render(body, args);
+export const writeTools = (tools: any) => {
+  fs.writeFileSync(path.resolve(baseDir, "tools.json"), JSON.stringify(tools, null, 2), "utf8");
 };
 
-export const createExecutiveSummaryPage = async (args: any, options: { index: number; name: string }) => {
+const generateHtml = async (args: any, options: { index: number; name: string }) => {
   const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  const tmpName = name.replace(/^create/, "");
+  const fileName = tmpName.charAt(0).toLowerCase() + tmpName.slice(1)
+  await createPage(getOutfile(index), nunjucks.render(filePath(fileName), args));
+};
+export const createExecutiveSummaryPage = async (args: any, options: { index: number; name: string }) => {
+  await generateHtml(args, options);
 };
 
 export const createSectionDividerPage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
 export const createScqaPage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
 export const createAgendaPage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
 export const createPyramidPrinciplePage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
 export const createIssueTreePage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
 export const createThankYouContactPage = async (args: any, options: { index: number; name: string }) => {
-  const { index, name } = options;
-  await createPage(getOutfile(index), renderPage(filePath(name), args));
+  await generateHtml(args, options);
 };
